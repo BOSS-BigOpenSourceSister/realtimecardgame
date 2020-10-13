ce79fca111a645c2719ae23f50e2ed09973ec1a2
Thamires <thamiresdepontesdasilva@gmail.com>
2020-10-13 20:55:51 -0300

    Atualiza Animator
    Co-authored-by: Sara Silva <saracsas2@gmail.com>

---
+++ Assets/Gameplay/Behaviours/ProjectileAttackBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Gameplay.Behaviours.Interfaces;
+using UnityEngine;
+
+public class ProjectileAttackBehaviour : IAttacker
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        [SerializeField] GameObject Projectile;
+
+    }
+
+    void Awake()
+    {
+        
+        Projectile.Add((GameObject)Instantiate(Projectile, Projectile.transform.position, Projectile.transform.rotation));
+
+    }
+
+
+    public void Attack(IDamageable target)
+    {
+        throw new NotImplementedException();
+    }
+}
