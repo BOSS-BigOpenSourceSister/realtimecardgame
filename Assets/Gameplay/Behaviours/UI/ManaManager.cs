a1e5fc4fee433070e92e16276ecfce08d5136404
Amanda Bezerra <amandabs_contato@hotmail.com>
2020-10-14 17:24:33 -0300

    Add UI texts with amount and insufficiency mana information

--- Assets/Gameplay/Behaviours/UI/ManaManager.cs
+++ Assets/Gameplay/Behaviours/UI/ManaManager.cs
@@ -1,15 +1,31 @@
 ﻿using UnityEngine;
 using Gameplay.Behaviours;
+using UnityEngine.UI;
 
 
 public class ManaManager
 {
     public static int manaMaxValue = 12;
-    public int mana = manaMaxValue;
+    public int mana = 0;
     public float timeRemaining = 10;
 
     public void UseCard(ManaBehaviour manaBehaviour) {
-        mana -= manaBehaviour.CardCost;
+        if (mana < manaBehaviour.CardCost)
+        {
+            ShowLabelManaWarning(true);
+        }
+        else
+        {
+            mana -= manaBehaviour.CardCost;
+
+            if (mana < 0)
+            {
+                mana = 0;
+            }
+
+            ShowLabelManaWarning(false);
+            UpdateLabelMana(mana);
+        }
     }
 
     public void AddMana()
@@ -24,10 +40,31 @@ public class ManaManager
             if (mana < manaMaxValue)
             {
                 mana += 1;
+
+                UpdateLabelMana(mana);
+
                 Debug.Log("Update Mana: " + mana);
             }
         }
     }
 
-    
+    public void UpdateLabelMana(int currentMana)
+    {
+        Text labelMana = GameObject.Find("LabelMana").GetComponent<Text>();
+
+        if (labelMana)
+        {
+            labelMana.text = "Manas: " + $"{currentMana}";
+        }
+    }
+
+    public void ShowLabelManaWarning(bool status)
+    {
+        Text labelManaWarning = GameObject.Find("LabelManaWarning").GetComponent<Text>();
+
+        if (labelManaWarning)
+        {
+            labelManaWarning.text = status ? "Sem mana suficiente" : "";
+        }
+    }
 }
