86925208d266ecbdd6d018c3c49a158a89b3f5b8
Thamires <thamiresdepontesdasilva@gmail.com>
2020-10-24 17:45:30 -0300

    Faz flecha acompanhar a animação

---
+++ Assets/ArcherAnimation.cs
@@ -0,0 +1,19 @@
+﻿using Gameplay.Behaviours;
+using UnityEngine;
+
+public class ArcherAnimation : MonoBehaviour
+{
+    private ProjectileAttackBehaviour projectileAttackBehaviour;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        projectileAttackBehaviour = gameObject.GetComponentInParent<ProjectileAttackBehaviour>();
+    }
+
+ 
+    public void InstantiateArrow()
+    {
+        projectileAttackBehaviour.Instantiated();
+    }
+}
--- Assets/Gameplay/Behaviours/ProjectileAttackBehaviour.cs
+++ Assets/Gameplay/Behaviours/ProjectileAttackBehaviour.cs
@@ -10,25 +10,25 @@ namespace Gameplay.Behaviours
      * Classe: ProjectileAttackBehaviour
      * Responsabilidade: Instanciar a flecha e setar um time para o Arqueiro
      */
-    public class ProjectileAttackBehaviour : BaseBehaviour, IAttacker
+    public class ProjectileAttackBehaviour : BaseBehaviour
     {
         //Deixando visivel no Unity para linkar com o prefab flecha e dizer a posição que a flecha deverá ser instanciada
         [SerializeField] ProjectileBehaviour projectile;
         [SerializeField] Transform positionProjectile; //Posição que meu objeto será instanciado
 
-        public void Attack(IDamageable target)
-        {
-            Debug.Log("Entramos");
-            Instantiated();
-                    
-        }
+        // public void Attack(IDamageable target)
+        // {
+        //     Debug.Log("Entramos");
+        //     //Instantiated();
+        //             
+        // }
         
         void SetProjectileTime(ProjectileBehaviour projectileArrow, Team team)
         {
             projectileArrow.Entity.Team = team;
         }
 
-        void Instantiated()
+        public void Instantiated()
         {
             var projectileArrow = Instantiate(projectile, positionProjectile);
                 SetProjectileTime(projectileArrow, Entity.Team);
